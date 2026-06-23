a06183bffd291b22c9b0ade6a1f57261b6147c71
Ghufran Malik <ghufranmalic@users.noreply.github.com>
2026-06-23 21:05:16 +0500

    Shrink-wrap Pages widget output

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/appt-widget-for-AR/",
+  build: {
+    rollupOptions: {
+      input: "src/index.html",
+    },
+  },
   plugins: [react()],
 });
